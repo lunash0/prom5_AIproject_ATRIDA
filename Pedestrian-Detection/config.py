984b77440af9348b26b127429238808263f31736
finallyupper <dianaoh1021@gmail.com>
2024-07-05 12:40:52 +0900

    feat: test on video and track by wandb

--- Pedestrian-Detection/config.py
+++ Pedestrian-Detection/config.py
@@ -1,7 +1,26 @@
+import wandb 
+
 BATCH_SIZE = 4
 SEED = 41
 IMG_SIZE = 512
 EPOCHS = 10
 NUM_CLASS = 2 
-LR = 3e-4
-MODEL_NAME = 'model'
+LR = 0.005
+MODEL_NAME = 'fasterrcnn'
+
+API_KEY = None 
+RUN = 0 
+
+def get_log():
+    wandb.login()
+    wandb.init(project='pedestrian_detection',
+               entity='finally_upper',
+               name = f'exp_{RUN}',
+               config={
+                    "batch_size": BATCH_SIZE,
+                    "img_size": IMG_SIZE,
+                    "epochs": EPOCHS,
+                    "num_classes": NUM_CLASS,
+                    "learning_rate": LR,
+                    "model_name": MODEL_NAME
+               })
